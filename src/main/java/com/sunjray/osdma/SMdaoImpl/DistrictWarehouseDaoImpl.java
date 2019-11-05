38a2427baf0f6dc32c2bde6f64e0a8e25c30aa6a
smruti <smruti.rout@ekutirsb.com>
2019-11-05 22:00:43 +0530

    commit by smruti

--- src/main/java/com/sunjray/osdma/SMdaoImpl/DistrictWarehouseDaoImpl.java
+++ src/main/java/com/sunjray/osdma/SMdaoImpl/DistrictWarehouseDaoImpl.java
@@ -20,13 +20,13 @@ public class DistrictWarehouseDaoImpl implements DistrictWarehouseDao {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
-	private static final String FETCH_PRODUCT_LIST = "SELECT mp.product_name, qpc.product_code_gen, qpc.product_status, wbp.quantity FROM osdma.t_os_warhouseb_product_list wbp left outer join osdma.t_os_qaqc_product_code qpc on wbp.qaqc_id = qpc.productcode_id left outer join osdma.t_os_master_product mp on qpc.product_id = mp.product_id order by wbp.product_list_id desc";
+	private static final String FETCH_PRODUCT_LIST = "SELECT mp.product_name, qpc.product_code_gen, wbp.status, wbp.quantity FROM osdma.t_os_warhouseb_stock_out wbp left outer join osdma.t_os_qaqc_product_code qpc on wbp.product_code_id = qpc.productcode_id left outer join osdma.t_os_master_product mp on qpc.product_id = mp.product_id order by wbp.wh_b_stock_out_id desc";
 
 	private static final String FETCH_STOCK_OUT_PRODUCT_LIST = "SELECT mp.product_name, qpc.product_code_gen, wbs.mat_request_id, wbs.quantity,wbs.wh_b_stock_out_id, wbs.stroge_location FROM osdma.t_os_warhouseb_stock_out wbs left outer join osdma.t_os_qaqc_product_code qpc on wbs.product_code_id = qpc.productcode_id left outer join osdma.t_os_master_product mp on qpc.product_id = mp.product_id order by wbs.mat_request_id desc";
 
 	private static final String UPDATE_STORAGE_LOCATION = "update osdma.t_os_warhouseb_stock_out set stroge_location = ? where wh_b_stock_out_id = ?";
 	
-	private static final String FETCH_PRODUCT_REPORT_LIST = "SELECT mp.product_name, qpc.product_code_gen, wbp.location_name, wbp.quantity FROM osdma.t_os_warhouseb_product_list wbp left outer join osdma.t_os_qaqc_product_code qpc on wbp.qaqc_id = qpc.productcode_id left outer join osdma.t_os_master_product mp on qpc.product_id = mp.product_id where qpc.product_status ='RECIEVED' order by wbp.product_list_id desc";
+	private static final String FETCH_PRODUCT_REPORT_LIST = "SELECT mp.product_name, qpc.product_code_gen, wbp.stroge_location, wbp.quantity FROM osdma.t_os_warhouseb_stock_out wbp left outer join osdma.t_os_qaqc_product_code qpc on wbp.product_code_id = qpc.productcode_id left outer join osdma.t_os_master_product mp on qpc.product_id = mp.product_id where wbp.status ='RECIVED' order by wbp.wh_b_stock_out_id desc";
 
 	private static final String FETCH_STOCK_OUT_REPORT_LIST = "SELECT pc.product_code_gen, mp.product_name, lsc.site_name as from_location, sc.site_name as to_location, ws.created_date, ws.quantity FROM osdma.t_os_dist_warhouse_stock_out ws left outer join osdma.t_os_am_consignment_logistic cl on ws.material_req_id = cl.mat_request_id left outer join osdma.t_os_qaqc_product_code pc on ws.product_code_id = pc.productcode_id left outer join osdma.t_os_sitecode sc on ws.site_id = sc.sitecode_id left outer join osdma.t_os_master_product mp on pc.product_id = mp.product_id left outer join osdma.t_os_sitecode lsc on cl.movement_location = lsc.sitecode_id order by ws.created_date desc";
 
